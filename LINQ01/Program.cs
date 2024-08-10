9e6268e86e700a02a313739753ae6ffeefe1fa93
DiabloXV <haboali1001@gmail.com>
2024-08-10 12:56:01 +0300

    Add project files.

---
+++ LINQ01/Program.cs
@@ -0,0 +1,14 @@
+﻿using Day_01_G03;
+using static Day_01_G03.ListGenerator;
+namespace LINQ01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
